2b8105323874d7add9da06532a21e5223d91e42f
Jack Rathwell <jrathwell@lcs.on.ca>
2024-12-13 14:38:42 -0500

    Initial Commit

---
+++ ShapesExercises/ShapesExercisesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapesExercisesApp.swift
+//  ShapesExercises
+//
+//  Created by Jack William Rathwell on 2024-12-13.
+//
+
+import SwiftUI
+
+@main
+struct ShapesExercisesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
